6ec8a34022dcc32a673ad14b958b904d536aceb7
tiwarishivani0204-blip <tiwarishivani0204@gmail.com>
2026-08-01 05:18:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (51.40%) - LeetSync

--- 1797-goal-parser-interpretation/goal-parser-interpretation.cpp
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     string interpret(string command) {
         string ans;
-        for(int i = 0 ; i < command.size(); i++){
-            if(command[i] == 'G'){
+        for (int i = 0; i < command.size(); i++) {
+            if (command[i] == 'G') {
                 ans += 'G';
-            }
-            else if(command[i] == ')' && command[i-1] == '('){
-                ans +='o';
-            }
-            else if(command[i] == 'a'){
+            } else if (command[i] == ')' && command[i - 1] == '(') {
+                ans += 'o';
+            } else if (command[i] == 'a') {
                 ans += "al";
             }
-        } 
-        return ans;  
+        }
+        return ans;
     }
 };
