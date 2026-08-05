6cd6da919b35acdf1a8d9d4e59032e5fb1901be9
tiwarishivani0204-blip <tiwarishivani0204@gmail.com>
2026-08-05 12:36:43 +0530

    Time: 2 ms (56.30%) | Memory: 7.9 MB (75.08%) - LeetSync

--- 374-guess-number-higher-or-lower/guess-number-higher-or-lower.cpp
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.cpp
@@ -13,7 +13,7 @@ public:
         int start = 0 ; 
         int end = n ;
         while(start <= end){
-            int mid = start + (end - start)/2;
+            int mid = start +(end - start)/2;
             if(guess(mid) == 0){
                 return mid;
             }
